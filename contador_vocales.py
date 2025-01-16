6b2931d9b2e18bdb1c7eadd78d4f87765069cb19
marianacrmz <marianacramirez.18@gmail.com>
2025-01-15 21:37:00 -0600

    Actividad

---
+++ contador_vocales.py
@@ -0,0 +1,16 @@
+# Solicitar una oración al usuario
+oracion = input("Por favor ingresa una oracion: ")
+
+# Definir las vocales
+vocales = "aeiouAEIOU"
+
+# Contador de vocales
+contador = 0
+
+# Recorrer cada caracter de la oración
+for caracter in oracion:
+    if caracter in vocales:
+        contador += 1
+
+# Resultado
+print(f"La oracion ingresada tiene {contador} vocales")
